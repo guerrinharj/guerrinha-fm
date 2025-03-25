ed9ad8cbe1273f109fb6f24fe6c1eb9cfea3064a
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-03-25 13:33:33 -0300

    seed wagner

--- guerrinha_radio_api/tracks/management/commands/seed.py
+++ guerrinha_radio_api/tracks/management/commands/seed.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from tracks.models import Track
 
 class Command(BaseCommand):
-    help = 'Seed the database with a test track'
+    help = 'Seed the database with test tracks'
 
     def handle(self, *args, **kwargs):
         Track.objects.all().delete()
@@ -16,4 +16,67 @@ class Command(BaseCommand):
             cover_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/cover.png"
         )
 
-        self.stdout.write(self.style.SUCCESS("✅ Database seeded with track 'Chalé'"))
+        Track.objects.create(
+            name="Edifício",
+            album="Wagner",
+            year=2018,
+            song_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/Guerrinha%20-%20Wagner%20-%2002%20Edifi%CC%81cio.mp3",
+            album_url="https://www.gpgc.info/releases/wagner",
+            cover_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/cover.png"
+        )
+
+        Track.objects.create(
+            name="Gazebo",
+            album="Wagner",
+            year=2018,
+            song_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/Guerrinha%20-%20Wagner%20-%2003%20Gazebo.mp3",
+            album_url="https://www.gpgc.info/releases/wagner",
+            cover_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/cover.png"
+        )
+
+        Track.objects.create(
+            name="Rancho",
+            album="Wagner",
+            year=2018,
+            song_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/Guerrinha%20-%20Wagner%20-%2004%20Rancho.mp3",
+            album_url="https://www.gpgc.info/releases/wagner",
+            cover_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/cover.png"
+        )
+
+        Track.objects.create(
+            name="Auditório",
+            album="Wagner",
+            year=2018,
+            song_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/Guerrinha%20-%20Wagner%20-%2005%20Audito%CC%81rio.mp3",
+            album_url="https://www.gpgc.info/releases/wagner",
+            cover_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/cover.png"
+        )
+
+        Track.objects.create(
+            name="Mirante",
+            album="Wagner",
+            year=2018,
+            song_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/Guerrinha%20-%20Wagner%20-%2006%20Mirante.mp3",
+            album_url="https://www.gpgc.info/releases/wagner",
+            cover_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/cover.png"
+        )
+
+        Track.objects.create(
+            name="Bosque",
+            album="Wagner",
+            year=2018,
+            song_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/Guerrinha%20-%20Wagner%20-%2007%20Bosque.mp3",
+            album_url="https://www.gpgc.info/releases/wagner",
+            cover_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/cover.png"
+        )
+
+        Track.objects.create(
+            name="Pomar",
+            album="Wagner",
+            year=2018,
+            song_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/Guerrinha%20-%20Wagner%20-%2008%20Pomar.mp3",
+            album_url="https://www.gpgc.info/releases/wagner",
+            cover_url="https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Wagner/cover.png"
+        )
+
+        self.stdout.write(self.style.SUCCESS("✅ Database seeded with tracks"))
