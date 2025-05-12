8e4055ca6724a322bd29aaa5b18add851f4b2ab6
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-05-11 22:44:54 -0300

    fixed channel layers

--- guerrinha_fm_api/guerrinha_fm/settings.py
+++ guerrinha_fm_api/guerrinha_fm/settings.py
@@ -139,3 +139,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
